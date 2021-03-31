f1e386cd45f2e66bf5fa0bd843b638edc714582a
diyaa <diyaaine@gmail.com>
2021-03-31 02:06:08 -0400

    Calculating feels like temperature

---
+++ src/com/diyamolVarghese_c0787971_a8/FeelLikeCalculator.java
@@ -0,0 +1,18 @@
+package com.diyamolVarghese_c0787971_a8;
+import java.util.Arrays;
+public class FeelLikeCalculator {
+    private static String[] days= {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+
+    public static int calculateFeelsLikeTemp(String day,int windSpeed,int temp){
+        int feelsLikeTemp=0;
+            if(Arrays.stream(days).anyMatch(t -> t.equals(day))){
+                feelsLikeTemp=temp*windSpeed;
+            }
+            else
+            {
+                feelsLikeTemp=10000;
+            }
+        return feelsLikeTemp;
+    }
+}
+
